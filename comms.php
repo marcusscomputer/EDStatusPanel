81e79ce83f593c5788f239625415971813393584
marcus-s <youtube@marcus-s.de>
2021-05-05 16:03:43 +0200

    Update comms.php
    Made the colors more readible

--- comms.php
+++ comms.php
@@ -18,9 +18,9 @@ function ED_getComms()
 				str_contains($data[$i]["Message"], "STATION_docking_granted") == true ||
 				str_contains($data[$i]["Message"], "STATION_NoFireZone_exited") == true
 			)
-			{ echo "<font color=#999900>[ ".$data[$i]["From"]." ] </b><font color=#00EE77>".$data[$i]["Message_Localised"]."</font></b><br>"; }
+			{ echo "<font color=#5383de>[ ".$data[$i]["From"]." ] </b><font color=#00EE77>".$data[$i]["Message_Localised"]."</font></b><br>"; }
 			else
-			{ echo "<font color=#009999>[ ".$data[$i]["From"]." ] : </b><font color=#00EE77>".$data[$i]["Message"]."</font></b><br>"; }
+			{ echo "<font color=#cf6f0c>[ ".$data[$i]["From"]." ] : </b><font color=#00EE77>".$data[$i]["Message"]."</font></b><br>"; }
 		}
 	}
 }
