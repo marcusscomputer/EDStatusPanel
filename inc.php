837913c6a44cf5f4b341352c725b10d7a0f702b3
JanoPL <>
2021-05-08 20:35:05 +0200

    fixed apache configuration and path to ED dir - updated .env.examples

--- inc.php
+++ inc.php
@@ -1,5 +1,9 @@
 <?php
 
-$GLOBALS["ed_journal_folder"] = "C:\Users\marcus-s\Saved Games\Frontier Developments\Elite Dangerous";
+if (file_exists('./Elite')) {
+    $GLOBALS["ed_journal_folder"] = './Elite';
+} else {
+    $GLOBALS["ed_journal_folder"] = "C:\Users\marcus-s\Saved Games\Frontier Developments\Elite Dangerous";
+}
 
 ?>
